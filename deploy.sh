33865943438c203a33f3684db87ecb35b3907da7
ReliaMM <1820184044@qq.com>
2020-03-20 22:26:14 +0800

    doc: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,22 @@
-#!/usr/bin/env sh
+#! /bin/sh
 
-# 构建项目
 npm run docs:build
 
-# 进入生成的文件夹
+git config --global user.name "$GIT_NAME"
+git config --global user.email "$GIT_EMAIL"
+
+if [ "$ROT_TOKEN" = "" ]; then
+  echo "Bye~"
+  exit 0
+fi
+
+git clone https://$ROT_TOKEN@github.com/ReliaMM/FHBlog.git
+npm run docs: build
 cd dist
 
-git config --global user.name "ReliaMM"
-git config --global user.email "1820184044@qq.com"
 git add -A .
 git commit -m 'deploy'
 
-git push -f git@github.com:ReliaMM/FHBlog.git master:gh-pages
+git push -f https://$ROT_TOKEN@github.com/ReliaMM/FHBlog.git master:gh-pages
+
+
